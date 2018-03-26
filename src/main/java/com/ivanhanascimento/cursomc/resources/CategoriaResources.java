9e23bd602f13193e58b96058e26e99ae7db21458
Ivan Assuncao <ivan_assuncao@hotmail.com>
2018-03-26 12:26:41 -0300

    Testando Domain Categoria

--- src/main/java/com/ivanhanascimento/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/ivanhanascimento/cursomc/resources/CategoriaResources.java
@@ -1,17 +1,29 @@
 package com.ivanhanascimento.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ivanhanascimento.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
+	public List<Categoria> listar() {
 		
-		return "Rest está funcionando!";
+		Categoria Cat1 = new Categoria(1,"Informática");
+		Categoria Cat2 = new Categoria(2,"Financeiro");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(Cat1);
+		lista.add(Cat2);
+		
+		return lista;
 	}
 	
 }
